bc707247df6340fc109f58afa302ff7fc8c3d8f8
Sara Olsson <sara.f.olsson@gmail.com>
2016-05-11 18:28:36 +0200

    Merge remote-tracking branch 'origin/Emmas-ändringar'
    # Conflicts:
    #	views/index.tpl

--- Server.py
+++ Server.py
@@ -1,9 +1,23 @@
 #coding:utf-8
 from bottle import *
+'''
 from beaker.middleware import SessionMiddleware
+
+import MySQLdb
 
 #OPTIONS
 #Log in to database
 db = MySQLdb.connect("195.178.232.16", port=3306, user="AF9250", passwd="Ankdamm1", db="AF9250", charset='utf8');
 cur = db.cursor(MySQLdb.cursors.DictCursor)
+# Här ligger alla routes
+'''
+@route('/static/<filename:path>')
+def server_static(filename):
+    return static_file(filename, root='static')
 
+@route("/")
+def index():
+    return template("index")
+
+
+run(host='localhost', port=8080, debug=True)
