4ca28eb7c15368d955fb1081ec484b35e4448488
Zachsian <emma.zachs@gmail.com>
2016-05-12 16:44:31 +0200

    Första försök att skriva ut från db

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 from bottle import *
-'''
+
 from beaker.middleware import SessionMiddleware
 
 import MySQLdb
@@ -9,8 +9,16 @@ import MySQLdb
 #Log in to database
 db = MySQLdb.connect("195.178.232.16", port=3306, user="AF9250", passwd="Ankdamm1", db="AF9250", charset='utf8');
 cur = db.cursor(MySQLdb.cursors.DictCursor)
+
+def get_people():
+    query = ("SELECT Nr, Name, Phone FROM employees")
+    cur.execute(query)
+    return cur.fetchall()
+
+
+
 # Här ligger alla routes
-'''
+
 @route('/static/<filename:path>')
 def server_static(filename):
     return static_file(filename, root='static')
@@ -19,5 +27,8 @@ def server_static(filename):
 def index():
     return template("index")
 
+@route("/admin")
+def admin():
+    return template("admin", employees=get_people())
 
 run(host='localhost', port=8080, debug=True)
